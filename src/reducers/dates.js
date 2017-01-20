0d06d5f5223aef191bdc9be6d5c1d701e35265f3
Øyvind Aaraas <Oyvind.Aaraas@klp.no>
2017-01-20 23:19:12 +0100

    feat: place date in correct array position

--- src/reducers/dates.js
+++ src/reducers/dates.js
@@ -14,11 +14,21 @@ const addDate = (state, action) => {
 const dates = (state = [], action) => {
   switch (action.type) {
     case 'ADD_DATE':
-      const a = [
-        ...state,
+
+      //get index to add within state
+      let index = state.findIndex((item) => {
+        return item.range.start.isAfter(action.range.start);
+      });
+
+      if(index < 0) {
+        index = state.length;
+      }
+      return [
+        ...state.slice(0, index),
         addDate(undefined, action),
+        ...state.slice(index),
       ];
-      return a;
+
     default:
       return state;
   }
